5729c5ef1b98275f35142f0ff80947834bb2026e
fer0o <fer0osennin@gmail.com>
2022-09-19 15:48:15 -0500

    add navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className='bg-black text-white text-2xl flex justify-center p-8 '>
+        Prediccion Meteorológica
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
