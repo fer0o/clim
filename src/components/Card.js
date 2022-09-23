3d491d19e70851446db857016958d7bb8d355c5c
fer0o <fer0osennin@gmail.com>
2022-09-23 18:06:08 -0500

    cambios menores

--- src/components/Card.js
+++ src/components/Card.js
@@ -21,16 +21,18 @@ const Card = ({ showData, loadingData, weather, forecast }) => {
       <div>
         {showData === true ? (
           <div className=' container m-auto w-auto '>
-            <div className=' grid grid-cols-1 lg:grid-cols-2 gap-2 mb-4 '>
-              <div className='p-2 border-2  border-white'>
+            <div className=' grid grid-cols-1 lg:grid-cols-2 gap-2 mb-4  '>
+              <div className='p-2 border-2  border-white '>
                 <div className='block text-left   p-4  m-2  text-white font-bold  rounded-md absolute'>
-                  <h3 className='lg:p-8 lg:text-6xl '>{weather.name}</h3>
-                  <h3 className='lg:p-8 lg:text-6xl '>{weather.sys.country}</h3>
-                  <p className='lg:p-8 lg:text-6xl'>{date}</p>
-                  <h1 className='lg:p-8 lg:text-6xl'>
+                  <h3 className=' lg:text-6xl '>
+                    {weather.name} , {weather.sys.country}
+                  </h3>
+
+                  <p className='mt-4 lg:text-6xl'>{date}</p>
+                  <h1 className='mt-60 lg:text-8xl'>
                     {(weather.main.temp - 273.15).toFixed(1)}°C
                   </h1>
-                  <p className='lg:p-8 lg:text-6xl'>
+                  <p className=' lg:text-6xl'>
                     <img src={iconUrl} className='h-20' alt='icon' />
                     {weather.weather[0].description}
                   </p>
@@ -48,7 +50,9 @@ const Card = ({ showData, loadingData, weather, forecast }) => {
             <div></div>
           </div>
         ) : (
-          <h2>No hay información</h2>
+          <h2 className='block text-center text-3xl text-white p-96'>
+            No hay información
+          </h2>
         )}
       </div>
     </div>
